f57f00360eda2b1c19f72dce9504768befcd3392
Semih Turan <trnsmh@gmail.com>
2021-02-22 23:52:08 -0500

    NavBar Added CollectionView Added

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Semih Turan on 2/22/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
